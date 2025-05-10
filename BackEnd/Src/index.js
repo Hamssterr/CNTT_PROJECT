6ac0c0dd1f13beaa8ab458d2ecb1f49f961dede0
Hamssterr <128308841+Hamssterr@users.noreply.github.com>
2025-05-11 01:18:14 +0700

    Update index.js

--- BackEnd/Src/index.js
+++ BackEnd/Src/index.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 8080;
 
 // Cấu hình CORS
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://cntt-project-frontend.onrender.com",
   credentials: true,
   allowedHeaders: [
     "Content-Type",
