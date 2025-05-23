141a52e085a9e13da1e6e4cc8935e9b79d611c8d
Hamssterr <dancamu1002@gmail.com>
2025-05-23 14:13:09 +0700

    Upload API get timetable for student and parent

--- BackEnd/Src/controller/class.controller.js
+++ BackEnd/Src/controller/class.controller.js
@@ -415,3 +415,103 @@ export const getClassesByInstructor = async (req, res) => {
     });
   }
 };
+
+// API: Lấy danh sách lớp học của học sinh đăng nhập
+export const getClassByIdStudent = async (req, res) => {
+  try {
+    const studentId = req.user?.userId;
+
+    // Kiểm tra xem studentId có tồn tại không
+    if (!studentId) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized: Student ID not found",
+      });
+    }
+
+    // Kiểm tra xem studentId có hợp lệ không
+    if (!mongoose.isValidObjectId(studentId)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid Student ID",
+      });
+    }
+
+    // Tìm tất cả các lớp học mà student.id khớp với userId
+    const classes = await Class.find({ "students.userId": studentId })
+      .populate("courseId", "title")
+      .lean();
+
+    // Kiểm tra xem có lớp học nào không
+    if (!classes || classes.length === 0) {
+      return res.status(200).json({
+        success: true,
+        message: "No classes found for this student",
+        classes: [],
+      });
+    }
+
+    // Trả về danh sách lớp học
+    return res.status(200).json({
+      success: true,
+      message: "Classes retrieved successfully",
+      classes,
+    });
+  } catch (error) {
+    console.error("Error retrieving classes by instructor:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to retrieve classes",
+    });
+  }
+};
+
+export const getClassWithHaveChildren = async (req, res) => {
+  try {
+    const parents = await User.find({ role: "parent" }).select("children");
+
+    // Lấy danh sách tất cả children của parents
+    const studentId = parents
+      .flatMap((parent) => parent.children.map((child) => child.id.toString()))
+      .filter((id) => id);
+
+    // Tìm tất cả classes có chứa children trong mảng students
+    const classes = await Class.find({ "students.userId": studentId })
+      .populate("courseId", "title")
+      .lean();
+
+    // Kiểm tra lớp học có tồn tại children không
+    if (classes.length === 0) {
+      return res.status(404).json({
+        message: "No classes found with students who are children of parent.",
+      });
+    }
+
+    // Lọc kết quả chỉ chứa childrenId có trong thông tin parent thôi
+    const filterClasses = classes.map((classItem) => {
+      const filterStudents = classItem.students.filter((student) => {
+        const userId = student.userId.toString();
+        return studentId.includes(userId);
+      });
+      return {
+        ...classItem,
+        students: filterStudents,
+      };
+    });
+
+    // Trả kết quả
+    res.status(200).json({
+      message: "List of classes with students who are children of parent",
+      data: filterClasses,
+    });
+  } catch (error) {
+    console.error(
+      "Error Error retrieving classes by parent with have childrenId:",
+      error
+    );
+    return res.status(500).json({
+      success: false,
+      message: "Failed to retrieve classes",
+    });
+  }
+};
--- BackEnd/Src/routes/student.routes.js
+++ BackEnd/Src/routes/student.routes.js
@@ -1,10 +1,13 @@
-import express from 'express';
+import express from "express";
 
-import { verifyStudent } from '../middleware/verifyStudent.js';
-import {getDataStudent} from '../controller/student.controller.js'
+import { verifyStudent } from "../middleware/verifyStudent.js";
+import { getDataStudent } from "../controller/student.controller.js";
+import { getClassByIdStudent } from "../controller/class.controller.js";
 
 const router = express.Router();
 
-router.get('/dashboard', verifyStudent, getDataStudent)
+router.get("/dashboard", verifyStudent, getDataStudent);
+
+router.get("/getClassByIdStudent", verifyStudent, getClassByIdStudent);
 
 export default router;
